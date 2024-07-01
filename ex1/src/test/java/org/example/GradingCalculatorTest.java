f82e4c8c9d1e791d9375fa4834a9d1b459d8a9a2
Loick <loickwalle@gmail.com>
2024-07-01 14:10:23 +0200

    Refacto avec paramaterizedTest

--- ex1/src/test/java/org/example/GradingCalculatorTest.java
+++ ex1/src/test/java/org/example/GradingCalculatorTest.java
@@ -1,100 +1,27 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class GradingCalculatorTest {
 
     private GradingCalculator gradingCalculator;
 
-    @Test
-    public void TestGetGradeWhenScore_95_AndAttendance_90_ThenResult_A(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(95, 90);
-        char resultAwait = 'A';
-
-        //Act
+    @ParameterizedTest
+    @CsvSource({
+            "95, 90, A",
+            "85, 90, B",
+            "65, 90, C",
+            "95, 65, B",
+            "95, 55, F",
+            "65, 55, F",
+            "50, 90, F"
+    })
+    public void TestGetGrade(int score, int attendance, char expectedGrade){
+        gradingCalculator = new GradingCalculator(score, attendance);
         char result = gradingCalculator.getGrade();
 
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_85_AndAttendance_90_ThenResult_B(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(85, 90);
-        char resultAwait = 'B';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_65_AndAttendance_90_ThenResult_C(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(65, 90);
-        char resultAwait = 'C';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_95_AndAttendance_65_ThenResult_C(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(95, 65);
-        char resultAwait = 'B';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_95_AndAttendance_55_ThenResult_F(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(95, 55);
-        char resultAwait = 'F';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_65_AndAttendance_55_ThenResult_F(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(65, 55);
-        char resultAwait = 'F';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
-    }
-
-    @Test
-    public void TestGetGradeWhenScore_50_AndAttendance_90_ThenResult_F(){
-        //Arrange
-        gradingCalculator = new GradingCalculator(50, 90);
-        char resultAwait = 'F';
-
-        //Act
-        char result = gradingCalculator.getGrade();
-
-        //Assert
-        Assertions.assertEquals(resultAwait,result);
+        Assertions.assertEquals(expectedGrade,result);
     }
 }
